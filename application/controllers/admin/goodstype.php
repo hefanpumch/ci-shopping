9aeb03cbd97988da296e199d4764e88048f8f174
hefanpumch <hefanhuake@163.com>
2019-07-06 15:48:14 +1000

    Goods type management; Pagination

---
+++ application/controllers/admin/goodstype.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hefan
+ * Date: 2019-07-06
+ * Time: 11:40
+ */
+
+class goodstype
+{
+
+}
---
+++ application/models/goodstype_model.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hefan
+ * Date: 2019-07-06
+ * Time: 13:50
+ */
+
+class goodstype
+{
+
+}
